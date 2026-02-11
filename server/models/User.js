7ce52097ca78ff6c4f355130c61f062932e1e630
Claire Lee <jclairelee2@gmail.com>
2026-02-11 10:54:37 -0800

    Create user model db schema with validation

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createon: {
+    type: String,
+    default: () => new Date().toLocaleString(),
+  },
+});
+
+userSchema.index({ username: 1 }, { unique: true });
+
+module.exports = mongoose.model("User", userSchema);
